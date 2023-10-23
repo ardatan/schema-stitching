c317c1bc164784498601b774124c25eee676edf5
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-10-23 18:19:06 +0300

    Fixes https://github.com/the-guild-org/website/issues/1450

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,3 +1,12 @@
 import { withGuildDocs } from '@theguild/components/next.config';
 
-export default withGuildDocs();
+export default withGuildDocs({
+  redirects: () =>
+    Object.entries({
+      '/handbook/other-integrations/federation-services': '/handbook/other-integrations/federation-to-stitching-sdl',
+    }).map(([from, to]) => ({
+      destination: to,
+      permanent: true,
+      source: from,
+    })),
+});
