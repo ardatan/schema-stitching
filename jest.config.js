2d30379623fc66e12d5af0bc5772e4c8cf4d7356
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-07-15 18:10:35 +0300

    Add ESM_PACKAGES for handling ES modules
    Added ESM_PACKAGES array to specify packages for transformation.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ const CI = !!process.env.CI;
 
 const ROOT_DIR = __dirname;
 
+const ESM_PACKAGES = [
+  'uuid',
+];
+
 module.exports = {
   testEnvironment: 'node',
   rootDir: ROOT_DIR,
@@ -16,4 +20,7 @@ module.exports = {
     '^.+\\.ts?$': 'babel-jest',
     '^.+\\.js$': 'babel-jest',
   },
+  transformIgnorePatterns: [
+    `node_modules/(?!(${ESM_PACKAGES.join('|')})/)`,
+  ],
 };
